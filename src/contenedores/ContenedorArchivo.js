64ee1447d0f7b960aed025bfd5eb92b53d28b17b
zAnarchy <mendozagabriel726@gmail.com>
2023-01-23 19:26:35 -0500

    desafio terminado

--- src/contenedores/ContenedorArchivo.js
+++ src/contenedores/ContenedorArchivo.js
@@ -1,86 +1,72 @@
 import { promises as fs } from 'fs'
-import config from '../config.js'
-import util from 'util'
-
-function print(objeto) {
-    console.log(util.inspect(objeto, false, 12, true));
-}
 
 class ContenedorArchivo {
 
     constructor(ruta) {
-        this.ruta = `${config.fileSystem.path}/${ruta}`;
+        this.ruta = ruta;
     }
 
     async listar(id) {
-        const objs = await this.listarAll()
-        const buscado = objs.find(o => o.id == id)
+        const elems = await this.listarAll()
+        const buscado = elems.find(e => e.id == id)
         return buscado
     }
 
     async listarAll() {
         try {
-            const objs = await fs.readFile(this.ruta, 'utf-8')
-            // console.log(objs);
-            // //Definimos un esquemas de autores
-            // const authorSchema = new schema.Entity('authors',{}, {idAttribute:"mail"});
-            // const textSchema = new schema.Entity('text');
-            // const mensajeSchema = new schema.Entity('messages', {
-            //     author: authorSchema,
-            //     text: [textSchema]
-            // });
-            // const normalizedMessage = normalize(JSON.parse(objs), [mensajeSchema]);
-            // print(normalizedMessage);
-            return JSON.parse(objs)
+            const elems = await fs.readFile(this.ruta, 'utf-8')
+            return JSON.parse(elems)
         } catch (error) {
-            console.log(error);
             return []
         }
     }
 
-    async guardar(obj) {
-        const objs = await this.listarAll()
+    async guardar(elem) {
+        const elems = await this.listarAll()
+
         let newId
-        if (objs.length == 0) {
+        if (elems.length == 0) {
             newId = 1
         } else {
-            newId = objs[objs.length - 1].id + 1
+            newId = elems[elems.length - 1].id + 1
         }
-        const newObj = { ...obj, id: newId }
-        objs.push(newObj)
+
+        const newElem = { ...elem, id: newId }
+        elems.push(newElem)
+
         try {
-            await fs.writeFile(this.ruta, JSON.stringify(objs, null, 2))
-            return newObj
+            await fs.writeFile(this.ruta, JSON.stringify(elems, null, 2))
+            return newId
         } catch (error) {
             throw new Error(`Error al guardar: ${error}`)
         }
     }
 
     async actualizar(elem) {
-        const objs = await this.listarAll()
-        const index = objs.findIndex(o => o.id == elem.id)
+        const elems = await this.listarAll()
+        const index = elems.findIndex(e => e.id == elem.id)
         if (index == -1) {
-            throw new Error(`Error al actualizar: no se encontró el id ${id}`)
+            throw new Error(`Error al actualizar: no se encontró el id ${elem.id}`)
         } else {
-            objs[index] = elem
+            elems[index] = elem
             try {
-                await fs.writeFile(this.ruta, JSON.stringify(objs, null, 2))
+                await fs.writeFile(this.ruta, JSON.stringify(elems, null, 2))
             } catch (error) {
-                throw new Error(`Error al actualizar: ${error}`)
+                throw new Error(`Error al borrar: ${error}`)
             }
         }
     }
 
     async borrar(id) {
-        const objs = await this.listarAll()
-        const index = objs.findIndex(o => o.id == id)
+        const elems = await this.listarAll()
+        const index = elems.findIndex(e => e.id == id)
         if (index == -1) {
             throw new Error(`Error al borrar: no se encontró el id ${id}`)
         }
 
-        objs.splice(index, 1)
+        elems.splice(index, 1)
         try {
-            await fs.writeFile(this.ruta, JSON.stringify(objs, null, 2))
+            await fs.writeFile(this.ruta, JSON.stringify(elems, null, 2))
         } catch (error) {
             throw new Error(`Error al borrar: ${error}`)
         }
@@ -94,6 +80,5 @@ class ContenedorArchivo {
         }
     }
 }
-
 
 export default ContenedorArchivo
--- src/main.js
+++ src/main.js
@@ -1,124 +1,71 @@
 import express from 'express'
+import session from 'express-session'
+import MongoStore from 'connect-mongo'
+
+import config from './config.js'
+
 import { Server as HttpServer } from 'http'
-import { Server as IO } from 'socket.io'
-const { Router } = express
-import dbControler from './contenedores/dbControler.js'
+import { Server as Socket } from 'socket.io'
 
-//FAKER
-import faker from 'faker'
-faker.locale = 'es'
+import authWebRouter from './routers/web/auth.js'
+import homeWebRouter from './routers/web/home.js'
+import productosApiRouter from './routers/api/productos.js'
 
-// NORMALIZR
-import { normalize, schema } from 'normalizr';
-import util from 'util'
-function print(objeto) {
-    console.log(util.inspect(objeto, false, 12, true));
-}
-
-//Definimos un esquemas de autores
- const authorSchema = new schema.Entity('authors',{}, {idAttribute:"mail"});
- const textSchema = new schema.Entity('text');
- const mensajeSchema = new schema.Entity('messages', {
-     author: authorSchema,
-     text: [textSchema]
-});
-
-// Creamos esta funcion para listar los mensajes normalizados utilizando los metodos del contenedor 
-async function listarMensajesN() {
-    const archivoMensajes = await mensajesApi.listarAll()
-    const normalizados = normalizarMensajes(archivoMensajes)
-    print(normalizados)
-    return normalizados
-}
-const normalizarMensajes = (mensajesConId) => normalize(mensajesConId, [mensajeSchema])
-
-// Importo el DAOS
-import {
-    productosDao as productosApi,
-    mensajesDao as mensajesApi
-} from './daos/index.js'
+import addProductosHandlers from './routers/ws/productos.js'
+import addMensajesHandlers from './routers/ws/mensajes.js'
 
 //--------------------------------------------
 // instancio servidor, socket y api
+
 const app = express()
-const fileProducts = new dbControler("products")
-const fileMessages = new dbControler("messages")
-
-//--------------------------------------------
-// configuro el socket mensajes y productos
-
 const httpServer = new HttpServer(app)
-const io = new IO(httpServer)
-
-io.on('connection', async (socket) => {
-    console.log('nuevo cliente conectado');
-
-    //Mensajes 
-    //Utilizamos esta funcion para enviar los mensajes normalizados mediante el socket y que sean recibidos en el js del html
-    listarMensajesN()
-    .then((mensajesN)=>{
-        socket.emit('message', mensajesN)
-    })   
-    // una vez escuchamos al cliente y recibimos un mensaje, realizamos el envio a todos los demas pusheandolo a un array
-    socket.on('newMessage', async (data) => {
-        await mensajesApi.guardar(data);
-        listarMensajesN()
-        .then((res)=>{
-            io.sockets.emit('mensajes',res)
-        })
-        // re enviamos por medio broadcast los msn a todos los clientes que esten conectados en ese momento
-    })
-
-    //--------------------------------------------------------------------------------------------------------------------------
-
-    // productos
-    const products = await fileProducts.listAll();
-    // Le envio el historial de el array que ya tengo cuando un nuevo cliente se conecte
-    socket.emit('Products', products)
-
-    socket.on('newProduct', async (data) => {
-        await fileProducts.save(data);
-        const newProducts = await fileProducts.listAll();
-        io.sockets.emit("Products", newProducts);
-    })
-
-})
+const io = new Socket(httpServer)
 
 //--------------------------------------------
-// agrego middlewares
-const productosRouterTest = new Router()
+// configuro el socket
+
+io.on('connection', async socket => {
+    // console.log('Nuevo cliente conectado!');
+    addProductosHandlers(socket, io.sockets)
+    addMensajesHandlers(socket, io.sockets)
+});
+
+//--------------------------------------------
+// configuro el servidor
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
-app.use('/api/productos-test', productosRouterTest)
+
+app.set('view engine', 'ejs');
+
+app.use(session({
+    // store: MongoStore.create({ mongoUrl: config.mongoLocal.cnxStr }),
+    store: MongoStore.create({ mongoUrl: config.mongoRemote.cnxStr }),
+    secret: 'shhhhhhhhhhhhhhhhhhhhh',
+    resave: false,
+    saveUninitialized: false,
+    rolling: true,
+    cookie: {
+        maxAge: 60000
+    }
+}))
+
+//--------------------------------------------
+// rutas del servidor API REST
+
+app.use(productosApiRouter)
+
+//--------------------------------------------
+// rutas del servidor web
+
+app.use(authWebRouter)
+app.use(homeWebRouter)
 
 //--------------------------------------------
 // inicio el servidor
 
-const PORT = 8080
-const connectedServer = httpServer.listen(PORT, () => {
+const connectedServer = httpServer.listen(config.PORT, () => {
     console.log(`Servidor http escuchando en el puerto ${connectedServer.address().port}`)
 })
 connectedServer.on('error', error => console.log(`Error en servidor ${error}`))
-
-//-------------------------------------------------
-// Inicio faker
-
-function creaCombinacionesRandom() {
-    return {
-        nombre: faker.commerce.product(),
-        price: faker.commerce.price(),
-        thumbnail: faker.image.imageUrl()
-    }
-}
-
-productosRouterTest.get('/', async (req, res) => {
-    const productosRandom = []
-    for (let i = 0; i < 5; i++) {
-        productosRandom.push(creaCombinacionesRandom())
-    }
-    res.json(productosRandom)
-    // const productos = await productosApi.listarAll()
-    // res.json(productos)
-})
